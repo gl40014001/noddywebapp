932fa613daf9e1df9de4f630d674014a285a6a1c
skylarkwinops <jason@skylarkwinops.com>
2018-10-20 11:07:37 +0100

    yay first commit

---
+++ noddywebapp/Default.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace noddywebapp
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+
+            string myIP = IPNetworking.GetIP4Address();
+            lblServerIP.Text = myIP;
+        }
+    }
+
+
+    public class IPNetworking
+    {
+        public static string GetIP4Address()
+        {
+            string IP4Address = String.Empty;
+
+            foreach (IPAddress IPA in Dns.GetHostAddresses(HttpContext.Current.Request.UserHostAddress))
+            {
+                if (IPA.AddressFamily.ToString() == "InterNetwork")
+                {
+                    IP4Address = IPA.ToString();
+                    break;
+                }
+            }
+
+            if (IP4Address != String.Empty)
+            {
+                return IP4Address;
+            }
+
+            foreach (IPAddress IPA in Dns.GetHostAddresses(Dns.GetHostName()))
+            {
+                if (IPA.AddressFamily.ToString() == "InterNetwork")
+                {
+                    IP4Address = IPA.ToString();
+                    break;
+                }
+            }
+
+            return IP4Address;
+        }
+    }
+
+
+}
